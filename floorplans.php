dd5ced0aa186ecd06d5389840d7a9e75dc5fba11
kjaeel <kjaeel@icloud.com>
2020-03-03 21:33:41 +0530

    Add files
    Trial Run

---
+++ floorplans.php
@@ -0,0 +1,61 @@
+<?php include 'header.php';?>
+
+
+
+<br> <br> <br>
+<section id="tabs">
+  <div class="container">
+    <h6 class="section-title h1"></h6>
+    <div class="row">
+      <div class="col-xs-12 ">
+        <nav>
+          <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Premium 2BHK</a>
+            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Premium 1BHK</a>
+            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">2BHK</a>
+       <!--      <a class="nav-item nav-link" id="nav-about-tab" data-toggle="tab" href="#nav-about" role="tab" aria-controls="nav-about" aria-selected="false">About</a> -->
+          </div>
+        </nav>
+        <div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
+          <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+
+         <div class="row respotabs" >
+          <div class="col-sm-3 " > <img src="img/about.jpg" width="300"></div>
+           <div class="col-sm-3 resposivetab"> <img src="img/about.jpg" width="300"></div>
+           <div class="col-sm-3 resposivetab"> <img src="img/about.jpg" width="300"></div>
+
+         </div>
+
+          </div>
+          <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+           
+             <div class="row respotabs">
+          <div class="col-sm-3 " > <img src="img/about.jpg" width="300"></div>
+           <div class="col-sm-3 resposivetab"> <img src="img/about.jpg" width="300"></div>
+           <div class="col-sm-3 resposivetab"> <img src="img/about.jpg" width="300"></div>
+
+         </div>
+           
+          </div>
+          <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+         
+               <div class="row respotabs">
+          <div class="col-sm-3 " > <img src="img/about.jpg" width="300"></div>
+           <div class="col-sm-3 resposivetab"> <img src="img/about.jpg" width="300"></div>
+           <div class="col-sm-3 resposivetab"> <img src="img/about.jpg" width="300"></div>
+
+         </div>
+
+
+          </div>
+   
+        </div>
+      
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+<?php include 'footer.php';?>
---
+++ index.php
@@ -0,0 +1,131 @@
+<?php include 'header.php';?>
+
+
+ <!-- Header -->
+ <header class="restop">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+      <!-- Slide One - Set the background image for this slide in the line below -->
+      <div class="carousel-item active" style="background-image: url('img/about.jpg')">
+     <!--    <div class="carousel-caption d-none d-md-block">
+          <h2 class="display-4">First Slide</h2>
+          <p class="lead">This is a description for the first slide.</p>
+        </div> -->
+      </div>
+      <!-- Slide Two - Set the background image for this slide in the line below -->
+      <div class="carousel-item" style="background-image: url('https://source.unsplash.com/bF2vsubyHcQ/1920x1080')">
+     <!--    <div class="carousel-caption d-none d-md-block">
+          <h2 class="display-4">Second Slide</h2>
+          <p class="lead">This is a description for the second slide.</p>
+        </div> -->
+      </div>
+      <!-- Slide Three - Set the background image for this slide in the line below -->
+      <div class="carousel-item" style="background-image: url('https://source.unsplash.com/szFUQoyvrxM/1920x1080')">
+     <!--    <div class="carousel-caption d-none d-md-block">
+          <h2 class="display-4">Third Slide</h2>
+          <p class="lead">This is a description for the third slide.</p>
+        </div> -->
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+  </div>
+</header>
+
+
+<br> <br> <br>
+<!--features __. -->
+
+<div class="row" style="margin-right: 0px;">
+   <div class="col-xs-3 col-md-5" >
+   		
+   	
+           
+			<div class="text-center">
+              <img src="https://kshitijresidency.netlify.com/png/text_02.png" width="350">
+           <br> <br>
+              <span class="seperator">* * *</span>
+               </div>
+        <br> 
+        <div>
+            <p style="margin-left: 20px;">Great things never take shape from modest ambitions 
+              thats why we are pushing the limits and aiming for the sky.. Presenting  Kshitij Residency, a stellar project designed to inspire new trends in the neighbourhood. Situated next to 
+              <b> Mayur Colony </b> - One of the most desirable locations in Kothrud - <b> Kshitij Residency</b> is an upscale address offering quick-n-easy access to everything that matters. 
+              A handpicked location, contemporary design, best-in-class  
+              amenities and superb price advantage make it an irrestible proposition.  
+              <b> Kshitij Residency</b> is much more than your new home; its the horizon where aspiration meets fulfilment.
+
+              <br> <br>
+              <b>One of the biggesr composite project's in Gujraat colony &amp; Mayur colony locality</b> 
+            </p>
+        </div>
+         <!--    <p><a href="#" class="probootstrap-custom-link">About Us</a></p> -->
+       
+</div>
+    <div class="col-xs-9 col-md-71" style=" margin-top: 62px;">
+    	<img src="img/about.jpg" style="width: 100%">
+    </div>   
+  </div>
+
+  <br> <br> <br>  <br> <br>
+
+  <div class="row" style="margin-right: 0px;">
+   <div class="col-xs-3 col-md-5" >
+      
+    
+           
+      <div class="text-center">
+              <img src="https://kshitijresidency.netlify.com/png/text_03.png" width="300">
+           <br> <br>
+              <span class="seperator">* * *</span>
+               </div>
+        <br> 
+        <div style="margin-left: 20px;">
+           <p class="blendcolor"> POCKET-FRIENDLY HOMES</p>
+            <p class="alignblend">Adjacent to Mayur Colony but with an unbelivable price advantage! Now catch the buzz of mayur colony without stretching your budget!</p>
+
+
+                   <p class="blendcolor"> UNMATCHED CONNECTIVITY  </p>
+            <p class="alignblend"> Malls, Market, Schools &amp; College, garden, health clubs, <br> coffee shops, hospitals, swimming pools, jogging tracks <br>
+            and many other facilities /services are located close by. quick, easy access to the Pune-Banglore highway. </p>
+
+
+
+                   <p class="blendcolor"> ACCESS TO IT PARKS </p>
+            <p class="alignblend">Now spend less time on roads, more time at home! The Convenient and strategic location of <b> Kshitij Residency</b> 
+            results in smooth connectivity to th IT Parks of Hinjewadi/Kharadi/Magarpatta.</p>
+
+                   <p class="blendcolor"> CULTURAL EXUMBERANCE </p>
+            <p class="alignblend"> Excitement is never far away as the cultural and leisure hotspots of Kothrud are just a quick drive from home. </p>
+
+                   <p class="blendcolor"> Metro Magic </p>
+            <p class="alignblend"> The Proposed Paud road metro station is just round the corner. work on chandani chowk flyover is also in progress,
+            which will make travelling to Hinjewadi / Baner / Katraj easier and faster. </p>
+          
+
+        </div>
+         <!--    <p><a href="#" class="probootstrap-custom-link">About Us</a></p> -->
+       
+</div>
+    <div class="col-xs-9 col-md-71" style=" margin-top: 25%;">
+      <img src="img/about.jpg" style="width: 100%">
+    </div>   
+  </div>
+
+<br> <br>
+
+
+
+
+<?php include 'footer.php';?>
